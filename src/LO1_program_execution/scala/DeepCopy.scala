e571963c5258999c7306fa6bd184a3462e8668ee
pauled <drpauled@gmail.com>
2022-02-16 14:09:04 -0500

    Day8-class code

---
+++ src/LO1_program_execution/scala/DeepCopy.scala
@@ -0,0 +1,22 @@
+package LO1_program_execution.scala
+
+object DeepCopy {
+  def copyArray(arrIn:Array[Int]):Array[Int]={
+    var out:Array[Int]=Array.fill(arrIn.length)(0)
+    for (index <- arrIn.indices){
+      out(index)=arrIn(index)
+    }
+    out
+  }
+
+  def main(args: Array[String]): Unit = {
+    var arr1:Array[Int]=Array(3,9,27)
+    var arr2:Array[Int]=arr1
+    var arr3=copyArray(arr2)
+    arr2(1)=13
+    println("arr1: "+arr1.mkString(","))
+    println("arr2: "+arr2.mkString(","))
+    println("arr3: "+arr3.mkString(","))
+  }
+
+}
