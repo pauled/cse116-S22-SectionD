f2b13b0ba981f40a8932c791522c39f533870ac7
pauled <drpauled@gmail.com>
2022-03-16 12:49:50 -0400

    Day20-class code

--- src/LO3_functionalProgramming/FirstOrderFunctions.scala
+++ src/LO3_functionalProgramming/FirstOrderFunctions.scala
@@ -28,14 +28,70 @@ object FirstOrderFunctions{
     }
     out
   }
+  def selectionSortGeneral[T](inputData:Array[T],
+                    comparator:(T,T)=>Boolean):
+  Array[T]={
+    var out:Array[T]=inputData.clone()
+    var min:Int=0
+    var temp:T=out(0)
+
+    for (i<-out.indices){
+      min=i
+      for (j<-i+1 until out.length){
+        if (comparator(out(j),out(min))){
+          min=j
+        }
+      }
+      temp=out(i)
+      out(i)=out(min)
+      out(min)=temp
+    }
+    out
+  }
+  /*def distance(v1:PhysicsVector,v2:PhysicsVector):Double={
+    Math.sqrt(Math.pow(v1.x-v2.x,2)+
+      Math.pow(v1.y-v2.y,2)+
+      Math.pow(v1.z-v2.z,2))
+  }
+  def distanceComparator(referencePoint:PhysicsVector):
+      (PhysicsVector,PhysicsVector)=>Boolean={
+    distance(v1,referencePoint)<distance(v2,referencePoint)
+  }*/
   def main(args: Array[String]): Unit = {
+    /*val myWork:PhyicsVector=new PhysicsVector(0,0,0)
+    val myWorkComparator=distanceComparator(myWork)
+    val orderedHouses=selectionSortGeneral(houseList,myWorkComparator)*/
     var numbers=List(10,-7,-8,5,13)
     //var sorted=numbers.sorted
     //var sorted=numbers.sortWith(compare)
     var sorted=numbers.sortWith((a:Int,b:Int)=>a<b)
     println(sorted)
     var numbers2:Array[Int]=Array(5,3,4)
-    var out=selectionSort(numbers2,compare)
+    var out=selectionSortGeneral(numbers2,compare)
     println(out.mkString(" "))
+    var numbers3:Array[Double]=Array(2.0,4.0,1.0,0)
+    var out2=selectionSortGeneral(numbers3,(a:Double,b:Double)=> a<b)
+    println(out2.mkString(" "))
+    val compare2=(a:Double,b:Double)=> a<b
+    var out3=selectionSortGeneral(numbers3,compare2)
+    println(out3.mkString(" "))
+
+    var numbers4=List(10,-7,-8,5,13)
+    numbers4.foreach(println)
+    var words:List[String]=List("one","two","three","four")
+    var greater=(a:String)=>a.length>3
+    val filtered=words.filter(greater)
+    filtered.foreach(println)
+    val filtered2=words.filter(_.length>3)
+    filtered2.foreach(println)
+    var squared=numbers3.map(Math.pow(_,2.0))
+    squared.foreach(println)
+    //yield
+    //reduce
+    //fold
+    //sum
+    //distinct
+    //slice
+    //reverse
   }
 }
