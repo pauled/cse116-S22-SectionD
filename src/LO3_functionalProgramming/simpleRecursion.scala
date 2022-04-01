2bf654909c7ed4aabdd8b5004c77f45c352d0170
pauled <drpauled@gmail.com>
2022-04-01 14:22:22 -0400

    Day24-class code

--- src/LO3_functionalProgramming/simpleRecursion.scala
+++ src/LO3_functionalProgramming/simpleRecursion.scala
@@ -38,9 +38,51 @@ object simpleRecursion {
       fib(n-1)+fib(n-2)
     }
   }
+  def acronymTrail(strings: Array[String],
+                   loc:Int):String={
+    if (loc==strings.length){
+      ""
+    } else {
+      //strings(loc).slice(0,1)+acronymTrail(strings,loc+1)
+      var out:String=acronymTrail(strings,loc+1)
+      out=strings(loc).slice(0,1)+out
+      out
+    }
+  }
+  def acronymTail(strings: Array[String],
+                   loc:Int,acc:String):String={
+    if (loc==strings.length){
+      acc
+    } else {
+      acronymTail(strings,loc+1,
+        acc+strings(loc).slice(0,1))
+    }
+  }
+  def acronymTailHelper(strings:Array[String]):String={
+    acronymTail(strings,0,"")
+  }
+  def acronym(strings: Array[String]):String={
+    acronymTrail(strings,0)
+  }
+  def recursiveReverse(value: String):String={
+    if (value.length==0){
+      ""
+    } else {
+      recursiveReverse(value.slice(1,value.length))+
+        value.slice(0,1)
+    }
+  }
 
   def main(args: Array[String]): Unit = {
     var out=computeGeometricSum(4)
     println(out)
+    var words:Array[String]=Array("Fricked","Up",
+      "Beyond","All","Recognition")
+    var ac=acronym(words)
+    println(ac)
+    val ac2=acronymTailHelper(words)
+    println(ac2)
+    val rev:String=recursiveReverse("fun")
+    println(rev+" said")
   }
 }
